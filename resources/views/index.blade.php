624b4d7c860ce14a1102cbce5ea74027e26f14a9
leli-padilha <wellingson.sk82010@gmail.com>
2024-09-20 17:37:35 -0300

    ajustes finais

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -29,7 +29,7 @@
     </head>
     <body class="antialiased">
         <!-- Header -->
-        <header class="w-full bg-primary px-5 py-4">
+        <header class="w-full bg-secondary px-5 py-4">
             <div class="max-w-screen-xl m-auto flex justify-between items-center">
                 <div class="max-w-24 m-0 sm:max-w-28">
                     <img class="w-full" src="{{ asset('logos/logo_escrita.png') }}" alt="escrita logo marca encanto grazing food">
@@ -40,7 +40,7 @@
                             <path d="M19.05 5.06923C18.1331 4.14334 17.041 3.4092 15.8375 2.9096C14.634 2.40999 13.3431 2.15492 12.04 2.15923C6.58 2.15923 2.13 6.60923 2.13 12.0692C2.13 13.8192 2.59 15.5192 3.45 17.0192L2.05 22.1592L7.3 20.7792C8.75 21.5692 10.38 21.9892 12.04 21.9892C17.5 21.9892 21.95 17.5392 21.95 12.0792C21.95 9.42923 20.92 6.93923 19.05 5.06923ZM12.04 20.3092C10.56 20.3092 9.11 19.9092 7.84 19.1592L7.54 18.9792L4.42 19.7992L5.25 16.7592L5.05 16.4492C4.22755 15.1363 3.79092 13.6185 3.79 12.0692C3.79 7.52923 7.49 3.82923 12.03 3.82923C14.23 3.82923 16.3 4.68923 17.85 6.24923C18.6176 7.0131 19.2259 7.92178 19.6396 8.92256C20.0533 9.92335 20.2642 10.9963 20.26 12.0792C20.28 16.6192 16.58 20.3092 12.04 20.3092ZM16.56 14.1492C16.31 14.0292 15.09 13.4292 14.87 13.3392C14.64 13.2592 14.48 13.2192 14.31 13.4592C14.14 13.7092 13.67 14.2692 13.53 14.4292C13.39 14.5992 13.24 14.6192 12.99 14.4892C12.74 14.3692 11.94 14.0992 11 13.2592C10.26 12.5992 9.77 11.7892 9.62 11.5392C9.48 11.2892 9.6 11.1592 9.73 11.0292C9.84 10.9192 9.98 10.7392 10.1 10.5992C10.22 10.4592 10.27 10.3492 10.35 10.1892C10.43 10.0192 10.39 9.87923 10.33 9.75923C10.27 9.63923 9.77 8.41923 9.57 7.91923C9.37 7.43923 9.16 7.49923 9.01 7.48923H8.53C8.36 7.48923 8.1 7.54923 7.87 7.79923C7.65 8.04923 7.01 8.64923 7.01 9.86923C7.01 11.0892 7.9 12.2692 8.02 12.4292C8.14 12.5992 9.77 15.0992 12.25 16.1692C12.84 16.4292 13.3 16.5792 13.66 16.6892C14.25 16.8792 14.79 16.8492 15.22 16.7892C15.7 16.7192 16.69 16.1892 16.89 15.6092C17.1 15.0292 17.1 14.5392 17.03 14.4292C16.96 14.3192 16.81 14.2692 16.56 14.1492Z" fill="white"/>
                         </svg>
     
-                        <p class="text-slate-50 font-montserrat font-medium text-sm md:text-base">(54) 99125-7469</p>
+                        <p class="text-slate-50 font-montserrat font-semibold text-sm md:text-base">(54) 99125-7469</p>
                     </button>
                 </a>
             </div>
@@ -50,7 +50,7 @@
 
         <main>
              <!-- Banners -->
-            <section class="w-full pb-5 border-b border-primary md:pb-10">
+            <section class="w-full pb-5">
                 <div class="w-full">
                     <div class="relative overflow-hidden w-full mx-auto">
                         
@@ -58,21 +58,20 @@
                         <div class="flex transition-transform duration-1000 ease-in-out" id="carousel">
                             <!-- Slides do carrossel -->
                             <div class="w-full flex-shrink-0">
-                                <img src="{{ asset('images/banner1.jpg')}}" alt="Banner 1" class="w-full">
+                                <img src="{{ asset('images/banner_grazing.png')}}" alt="Banner 1" class="w-full">
                             </div>
                             <div class="w-full flex-shrink-0">
-                                <img src="{{ asset('images/banner2.webp')}}" alt="Banner 1" class="w-full">
+                                <img src="{{ asset('images/banner_grazing2.png')}}" alt="Banner 1" class="w-full">
                             </div>
-        
                         </div>
 
                         <!-- Botões de Navegação -->
-                        <button id="prevBtn" class="absolute top-1/2 left-4 transform -translate-y-1/2 text-white px-3 py-2 rounded-full">
+                        <button id="prevBtn" class="opacity-5 absolute top-1/2 left-4 transform -translate-y-1/2 text-white px-3 py-2 rounded-full">
                             <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" clip-rule="evenodd" d="M8.29279 12.707C8.10532 12.5195 8 12.2652 8 12C8 11.7348 8.10532 11.4805 8.29279 11.293L13.9498 5.636C14.042 5.54049 14.1524 5.46431 14.2744 5.4119C14.3964 5.35949 14.5276 5.3319 14.6604 5.33075C14.7932 5.3296 14.9248 5.3549 15.0477 5.40518C15.1706 5.45546 15.2823 5.52971 15.3762 5.6236C15.4701 5.7175 15.5443 5.82915 15.5946 5.95205C15.6449 6.07494 15.6702 6.20662 15.669 6.3394C15.6679 6.47218 15.6403 6.6034 15.5879 6.7254C15.5355 6.84741 15.4593 6.95775 15.3638 7.05L10.4138 12L15.3638 16.95C15.5459 17.1386 15.6467 17.3912 15.6445 17.6534C15.6422 17.9156 15.537 18.1664 15.3516 18.3518C15.1662 18.5372 14.9154 18.6424 14.6532 18.6447C14.391 18.647 14.1384 18.5462 13.9498 18.364L8.29279 12.707Z" fill="#272020"/>
                             </svg>
                         </button>
-                        <button id="nextBtn" class="absolute top-1/2 right-4 transform -translate-y-1/2 text-white px-3 py-2 rounded-full">
+                        <button id="nextBtn" class="opacity-5 absolute top-1/2 right-4 transform -translate-y-1/2 text-white px-3 py-2 rounded-full">
                             <svg width="44" height="44" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                 <g clip-path="url(#clip0_355_1183)">
                                     <path fill-rule="evenodd" clip-rule="evenodd" d="M15.7069 11.293C15.8943 11.4805 15.9996 11.7348 15.9996 12C15.9996 12.2652 15.8943 12.5195 15.7069 12.707L10.0499 18.364C9.95761 18.4595 9.84726 18.5357 9.72526 18.5881C9.60326 18.6405 9.47204 18.6681 9.33926 18.6693C9.20648 18.6704 9.0748 18.6451 8.9519 18.5948C8.829 18.5445 8.71735 18.4703 8.62346 18.3764C8.52957 18.2825 8.45531 18.1709 8.40503 18.048C8.35475 17.9251 8.32945 17.7934 8.3306 17.6606C8.33176 17.5278 8.35934 17.3966 8.41175 17.2746C8.46416 17.1526 8.54034 17.0422 8.63585 16.95L13.5859 12L8.63585 7.05C8.4537 6.8614 8.3529 6.6088 8.35518 6.3466C8.35746 6.0844 8.46263 5.83359 8.64804 5.64818C8.83344 5.46277 9.08426 5.35761 9.34645 5.35533C9.60865 5.35305 9.86125 5.45384 10.0499 5.636L15.7069 11.293Z" fill="#272020"/>
@@ -92,17 +91,17 @@
             <!-- Hero -->
             <section class="w-full px-4 py-4 md:px-10 md:py-10">
                 <div class="max-w-screen-xl m-auto flex flex-col justify-between gap-5 items-center md:flex-row md:gap-10">
-                    <div class="w-full flex flex-col gap-5 md:gap-10" data-aos="zoom-in-up">
-                        <div class="flex flex-col gap-2">
-                            <h1 class="font-lhcencoreregular text-center text-4xl font-medium md:text-start xl:text-4xl xl:text-justify">Transforme uma simples cesta de café da manhã em um momento especial!</h1>
-                            <p class="font-montserrat text-center text-sm tracking-wide md:text-start xl:text-base xl:text-start">
+                    <div class="w-full flex flex-col items-center gap-5 md:gap-10" data-aos="zoom-in-up">
+                        <div class="flex flex-col gap-3">
+                            <h1 class="font-lhcencoreregular text-center text-2xl font-medium xl:text-5xl xl:text-center text-pretty">Transforme uma simples cesta de café da manhã em um momento especial!</h1>
+                            <p class="font-montserrat text-center text-sm tracking-wide xl:text-base xl:text-center">
                                 <!-- Cestas encantadoras capazes de unir sabor e emoção em um único presente. </br> -->
                                 Criadas e entregues nas cidades de Marau, Vila Maria | RS. 
                             </p>
                         </div>
                         <a href="https://wa.me/5554991257469" target="_blank" class="w-full md:max-w-80">
-                            <button class="w-full bg-secondary px-4 py-4 rounded-2xl font-montserrat font-medium text-base flex align-center gap-1 justify-center shadow-md transform transition duration-300 ease-in-out hover:scale-110 md:max-w-80">
-                                <svg width="24" height="24" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg">
+                            <button class="w-full bg-secondary px-4 py-4 rounded-2xl font-montserrat font-medium text-sm md:text-base flex align-center gap-1 justify-center shadow-md transform transition duration-300 ease-in-out hover:scale-110 md:max-w-80">
+                                <svg width="20" height="20" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg">
                                     <path d="M19.1001 4.91002C18.1832 3.98413 17.0911 3.24999 15.8876 2.75039C14.6841 2.25078 13.3932 1.99571 12.0901 2.00002C6.6301 2.00002 2.1801 6.45002 2.1801 11.91C2.1801 13.66 2.6401 15.36 3.5001 16.86L2.1001 22L7.3501 20.62C8.8001 21.41 10.4301 21.83 12.0901 21.83C17.5501 21.83 22.0001 17.38 22.0001 11.92C22.0001 9.27002 20.9701 6.78002 19.1001 4.91002ZM12.0901 20.15C10.6101 20.15 9.1601 19.75 7.8901 19L7.5901 18.82L4.4701 19.64L5.3001 16.6L5.1001 16.29C4.27765 14.9771 3.84102 13.4593 3.8401 11.91C3.8401 7.37002 7.5401 3.67002 12.0801 3.67002C14.2801 3.67002 16.3501 4.53002 17.9001 6.09002C18.6677 6.85389 19.276 7.76257 19.6897 8.76335C20.1034 9.76414 20.3143 10.8371 20.3101 11.92C20.3301 16.46 16.6301 20.15 12.0901 20.15ZM16.6101 13.99C16.3601 13.87 15.1401 13.27 14.9201 13.18C14.6901 13.1 14.5301 13.06 14.3601 13.3C14.1901 13.55 13.7201 14.11 13.5801 14.27C13.4401 14.44 13.2901 14.46 13.0401 14.33C12.7901 14.21 11.9901 13.94 11.0501 13.1C10.3101 12.44 9.8201 11.63 9.6701 11.38C9.5301 11.13 9.6501 11 9.7801 10.87C9.8901 10.76 10.0301 10.58 10.1501 10.44C10.2701 10.3 10.3201 10.19 10.4001 10.03C10.4801 9.86002 10.4401 9.72002 10.3801 9.60002C10.3201 9.48002 9.8201 8.26002 9.6201 7.76002C9.4201 7.28002 9.2101 7.34002 9.0601 7.33002H8.5801C8.4101 7.33002 8.1501 7.39002 7.9201 7.64002C7.7001 7.89002 7.0601 8.49002 7.0601 9.71002C7.0601 10.93 7.9501 12.11 8.0701 12.27C8.1901 12.44 9.8201 14.94 12.3001 16.01C12.8901 16.27 13.3501 16.42 13.7101 16.53C14.3001 16.72 14.8401 16.69 15.2701 16.63C15.7501 16.56 16.7401 16.03 16.9401 15.45C17.1501 14.87 17.1501 14.38 17.0801 14.27C17.0101 14.16 16.8601 14.11 16.6101 13.99Z" fill="black"/>
                                 </svg>
                                 Entrar em contato
@@ -119,13 +118,13 @@
 
             <!-- Produtos -->
             <section class="w-full h-full pt-5 p-0 md:pt-10  bg-center bg-no-repeat bg-cover" style="background-image: url('{{asset('images/teste.jpg')}}')"> <!-- Section vai ter imagem de fundo -->
-                <div class="max-w-screen-xl m-auto flex sm:flex flex-col px-4 py-4 md:px-10 bg-white rounded-t-xl gap-5"> <!-- fundo branco -->
+                <div class="max-w-screen-xl m-auto flex border-t-2 border-l-2 border-r-2 border-secondary sm:flex flex-col px-4 py-4 md:px-10 bg-white rounded-t-xl gap-5"> <!-- fundo branco -->
                     <!-- Container titulo e subtitulo -->
                     <div class="w-full" data-aos="zoom-in">
-                        <h2 class="text-center font-montserrat text-xl font-semibold">
+                        <h2 class="text-center font-lhcencoreregular text-3xl xl:text-4xl font-bold">
                             Produtos
                         </h2>
-                        <h3 class="text-center font-lhcencoreregular font-semibold text-3xl">Encanto Grazing Food</h3>
+                        <h3 class="text-center font-montserrat text-sm xl:text-base font-medium">Encanto Grazing Food</h3>
                     </div>
                     <!-- Container das miniaturas -->
                     <div class="w-full grid grid-cols-1 gap-5 sm:grid-cols-2 md:gap-10">
@@ -186,31 +185,31 @@
 
             <!-- Sobre e depoimentos -->
             <section class="w-full pt-5">
-                <div class="flex flex-col gap-5 px-4 py-5 md:px-10 md:py-10 relative bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('images/fundosobre.jpg') }})">
+                <div class="flex flex-col gap-5 px-4 py-5 md:px-10 md:py-10 relative bg-cover bg-center bg-no-repeat" style="background-image: url({{ asset('images/sobre.png') }})">
                 
                     <div>
                         <div class="flex flex-col gap-2">
                             <h2 class="text-center font-lhcencoreregular font-semibold text-3xl text-white" data-aos="zoom-in-up">A Encanto Grazing Food</h2>
-                            <p class="max-w-3xl m-auto font-montserrat text-center text-white text-pretty tracking-wide" data-aos="zoom-in-up">A Encanto surgiu da necessidade de ter em nossa cidade uma opção de presente/comida afetiva, que fosse além de alimento, que entregasse amor, gratidão, pedido de desculpas, desejos de felicidades, enfim, que enchesse os olhos, a barriga e o coração de quem receber.</p>
+                            <p class="max-w-2xl m-auto font-montserrat text-center text-white text-pretty tracking-wide" data-aos="zoom-in-up">A Encanto surgiu da necessidade de ter em nossa cidade uma opção de presente/comida afetiva, que fosse além de alimento, que entregasse amor, gratidão, pedido de desculpas, desejos de felicidades, enfim, que enchesse os olhos, a barriga e o coração de quem receber.</p>
                         </div>
                     </div>
                     <div class="flex flex-col gap-2">
                         <h3 class="font-lhcencoreregular text-2xl text-center text-white font-semibold" data-aos="zoom-in-up">Depoimentos </h3>
                         <div class="max-w-7xl m-auto grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-4" data-aos="zoom-in-up">
                             <div class="bg-white p-5 text-center flex flex-col gap-4 rounded-2xl">
-                                <span class="font-lhcencoreregular text-xl font-semibold">Caroline</span>
+                                <span class="font-lhcencoreregular text-2xl font-semibold">Caroline</span>
                                 <p class="font-montserrat">“Maravilhosa, obrigada!</br> Virei cliente, sempre que precisar vou chamar!”</p>
                             </div>
                             <div class="bg-secondary p-4 text-center flex flex-col gap-4 rounded-2xl">
-                                <span class="font-lhcencoreregular text-xl font-semibold">Jessica</span>
+                                <span class="font-lhcencoreregular text-2xl font-semibold">Jessica</span>
                                 <p class="font-montserrat">“Ficou lindo e ela amou! Com certeza vou pegar mais vezes! Trabalho impecável. Atendimento de qualidade.”</p>
                             </div>
                             <div class="bg-white p-4 text-center flex flex-col gap-4 rounded-2xl">
-                                <span class="font-lhcencoreregular text-xl font-semibold">Danielle</span>
+                                <span class="font-lhcencoreregular text-2xl font-semibold">Danielle</span>
                                 <p class="font-montserrat">“Maravilhoso, simplesmente. Obrigada por além de entregar comida boa, entregar amor!”</p>
                             </div>
                             <div class="bg-secondary p-4 text-center flex flex-col gap-4 rounded-2xl">
-                                <span class="font-lhcencoreregular text-xl font-semibold">Cleide</span>
+                                <span class="font-lhcencoreregular text-2xl font-semibold">Cleide</span>
                                 <p class="font-montserrat">“Obrigada pelo carinho... Amei meu lanche que ainda se tornou café a manhã! Tudo uma delícia!”</p>
                             </div>
                         </div>
@@ -315,9 +314,9 @@
                 </div>
             </section>  
         </main>
-        <footer class="px-4 py-5" style="background-image: url({{asset('background.jpg')}})">
+        <footer class="px-4 py-5" style="background-image: url({{asset('images/footer.png')}})">
             <div class="flex flex-col gap-5" data-aos="zoom-in-up">
-                <div class="max-w-56 m-auto pr-8" >
+                <div class="max-w-40 m-auto pr-6" >
                     <img class="transform transition duration-300 ease-in-out hover:scale-110" src="{{asset('logos/logo.png')}}" alt="logo encanto grazing food">
                 </div>
                 <div class="flex justify-center gap-4 border-b-2 border-dashed pb-4">
