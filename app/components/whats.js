e5739e96327bd9011977142b4868c13d1c657d02
leli-padilha <wellingson.sk82010@gmail.com>
2024-10-21 10:42:34 -0300

    troca de banners

--- app/components/whats.js
+++ app/components/whats.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Whats(){
     return (
-        <a href="https://wa.me/5554992005646" target="_blank" className="fixed bottom-5 right-5 z-10">
+        <a href="https://wa.me/5554991257469" target="_blank" className="fixed bottom-5 right-5 z-10">
             <div className="transform transition duration-300 ease-in-out hover:scale-110">
                 <Image src="/images/whats.webp" alt="WhatsApp" width={64} height={64} />
             </div>
