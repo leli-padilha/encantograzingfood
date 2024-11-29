3ade5b74fab8158b5d3e7ff772d72895abc85c53
leli-padilha <wellingson.sk82010@gmail.com>
2024-11-29 09:40:57 -0300

    corrigido sintaxe again

--- app/layout.js
+++ app/layout.js
@@ -28,7 +28,7 @@ export default function RootLayout({ children }) {
                         src="https://www.googletagmanager.com/ns.html?id=GTM-N2LD8DFS"
                         height={0} 
                         width={0} 
-                        style={`display:none;visibility:hidden`}>
+                        style={{display:'none',visibility:'hidden'}}>
                     </iframe>
                 </noscript>
                 <AOSProvider>
