da79fef5069e795188ef6900648a7b24fa355cbb
leli-padilha <wellingson.sk82010@gmail.com>
2024-11-29 09:38:35 -0300

    ajustado sintaxe

--- app/layout.js
+++ app/layout.js
@@ -26,9 +26,9 @@ export default function RootLayout({ children }) {
                 <noscript>
                     <iframe 
                         src="https://www.googletagmanager.com/ns.html?id=GTM-N2LD8DFS"
-                        height="0" 
-                        width="0" 
-                        style="display:none;visibility:hidden">
+                        height={0} 
+                        width={0} 
+                        style={`display:none;visibility:hidden`}>
                     </iframe>
                 </noscript>
                 <AOSProvider>
